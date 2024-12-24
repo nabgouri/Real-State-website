92b31717dabd643f0eb1d4d0fb0ac8b062c3a4be
ayman nabgouri <nabgouri9@gmail.com>
2024-12-24 15:46:09 +0100

    fiximg some errors

--- src/Components/CustomerSection.jsx
+++ src/Components/CustomerSection.jsx
@@ -1,7 +1,7 @@
 import TextContent from "./TextContent";
 import Container from "./Container";
 import UlCard from "./UlCard";
-import { customersLogo } from "../util/CadsArrays";
+import { customersLogo } from "../util/cadsArrays";
 export default function CustomerSection() {
   return (
     <section className=" mb-[6.25rem]">
