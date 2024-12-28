2c7cad1cebfc71cfbd90b314b50365ce43d2eaf0
ayman nabgouri <nabgouri9@gmail.com>
2024-12-28 16:14:44 +0100

    adding the final gradients

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -2,21 +2,21 @@ import Container from "./Container";
 import TextContent from "./TextContent";
 import aboutTopImage from "../assets/images/about-images/about-top-img.png";
 import aboutBottomImage from "../assets/images/about-images/about-bottom-img.png";
-
+import afterDots from "../assets/images/plus-dots 1.png";
 export default function About() {
   return (
     <section>
       <Container className="md:flex items-center gap-[13.125rem] bg-[#F2F6F7] py-[7.5rem]">
-        <div className="flex">
+        <div className="flex after-class relative before-class">
           <img
             src={aboutBottomImage}
             alt="about-bottom-image"
-            className=" -translate-x-[7rem] translate-y-10"
+            className=" -translate-x-[47%] translate-y-[14%] z-10 "
           />
           <img
             src={aboutTopImage}
             alt="about-top-image"
-            className=" translate-x-full"
+            className=" translate-x-[90%]  z-10  "
           />
         </div>
         <TextContent
